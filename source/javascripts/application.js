188d4cd8abff6f7777b346d812092924d54dd8f8
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-15 17:45:14 +0100

    disabling default event of click

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,40 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+const disableClick = (event) => {
+  event.preventDefault();
+};
+
+const bindDisableClick = (element) => {
+  element.addEventListener("click", disableClick);
+};
+
+// const changeActiveTab = (event) => {
+//   event.
+// };
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".disable-click").forEach(bindDisableClick);
+
+  // document.querySelectorAll(".tab").forEach((tab) => {
+  // tab.addEventListener("click", (event) => {
+  //   document.querySelector(".active").classList.remove("active");
+  //   event.currentTarget.classList.add("active");
+  //   });
+  // });
+
+
+});
+
+// document.addEventListener = (element) => {
+
+// }
+
+
+// document.querySelectorAll(".tab").forEach((tab) => {
+//   tab.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("active");
+//   });
+// });
